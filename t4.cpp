9da61202f2d721f12944b26b99fb3674a7b9ebe9
batakhh <i252200@isb.nu.edu.pk>
2025-11-25 15:25:49 +0500

    Update t4.cpp

--- t4.cpp
+++ t4.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 void calculate(int *money, int total) {
     int values[4] = {5000, 1000, 500, 100};
-    
+
     for (int i = 0; i < 4; i++) {
         int count = total / values[i];
         if (count <= money[i]) {
@@ -14,25 +14,28 @@ void calculate(int *money, int total) {
 }
 
 int main() {
+    cout << "Enter bank notes: ";
     int bank[4];
-    
+
     for (int i = 0; i < 4; i++) {
         cin >> bank[i];
     }
-    
+
+    cout << "Enter amount needed: ";
     int need;
     cin >> need;
-    
+
     int *wallet = bank;
-    
+
     calculate(wallet, need);
-    
+
     wallet = nullptr;
-    
+
+    cout << "Remaining bank notes:" << endl;
     int types[4] = {5000, 1000, 500, 100};
     for (int i = 0; i < 4; i++) {
         cout << types[i] << " " << bank[i] << endl;
     }
-    
+
     return 0;
 }
