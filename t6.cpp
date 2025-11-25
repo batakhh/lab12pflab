ee7f193a8d94954b50809dcc3bd6b22ac8aab9a9
batakhh <i252200@isb.nu.edu.pk>
2025-11-25 13:52:12 +0500

    Update t6.cpp

--- t6.cpp
+++ t6.cpp
@@ -1,33 +1,34 @@
 #include <iostream>
 using namespace std;
 
-void borrowBooks(int **pp, int b[], int size) {
-    int *p = *pp;
+void borrowBooks(int *p, int b[], int size) {
     for (int i = 0; i < size; i++)
         p[i] -= b[i];
 }
 
 int main() {
     int stock[5], borrowArr[5];
+
     for (int i = 0; i < 5; i++)
         cin >> stock[i];
+
     for (int i = 0; i < 5; i++)
         cin >> borrowArr[i];
 
-    int *ptr = stock;
-
-    borrowBooks(&ptr, borrowArr, 5);
+    borrowBooks(stock, borrowArr, 5);
 
     for (int i = 0; i < 5; i++)
         cout << stock[i] << " ";
     cout << endl;
 
     int newStock[5];
+
     for (int i = 0; i < 5; i++)
         cin >> newStock[i];
 
-    ptr = newStock;
+    int *ptr = newStock;
 
     for (int i = 0; i < 5; i++)
         cout << ptr[i] << " ";
 }
+
